7dcef54d97cb6599105bc082483e1b843dee5c14
TobiPerel <pereltobias@gmail.com>
2024-03-22 10:38:49 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Algoritmos3
+
+Para hacer un ejecutable de c++ ejecutar el siguente comando : 
+
+```bash 
+g++ -o nombreEjecutable .\nombreArchivo.cpp
+
+```
---
+++ backtracking/sumarNumeros.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void findCombinations(vector<int>numeros,vector<int>&temporal,int index, int suma,int objetivo){
+
+    if(suma == objetivo){
+        cout << "Combinacion encontrada: ";
+        for (int j= 0; j < temporal.size(); j++){
+            cout << temporal[j] << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    for (int i=index;i<numeros.size();i++){
+        // if(i!= index && numeros[i]  ==  numeros[i-1]) continue; // cuando i= index significa que estas al comienzo de una rama y que no necesariamente el elemento que tenes antes lo analisaste.
+        if(suma + numeros[i]<= objetivo){
+            temporal.push_back(numeros[i]);
+            findCombinations(numeros,temporal,i+1,suma + numeros[i],objetivo);
+            temporal.pop_back() ; //saca el ultimo elemento. Hay dos opciones o llega a la objetivo o recorre todo el for, nunca llega entar al if y va para atras.
+        }
+        
+    }
+    
+}
+
+
+int main (){
+    int cantidad;
+    cin >> cantidad;
+    vector<int> numeros;
+    vector<int> temporal;
+    for (int i = 0; i < cantidad; i++){
+        int n;
+        cin >> n;
+        numeros.push_back(n);
+    }
+    int objetivo;
+    cin >> objetivo;
+    findCombinations(numeros,temporal,0,0,objetivo);
+    return 0;
+}
