2f8f6582df3fdc1bb1c981105c3bbb68a7092f70
TobiPerel <pereltobias@gmail.com>
2024-03-22 11:36:13 -0300

    avances

---
+++ backtracking/maradona.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void encontrarMejorFormacion(vector<string>&nombres,vector<int>ataque,vector<int>defensa, vector<string>&formacion,int sumaAtacantes, int sumaDefensas,int recordAtaque, int recordDefensa, int index){
+    if(index == 4 && sumaAtacantes>=recordAtaque){
+        if(sumaAtacantes==recordAtaque){
+            int defensaParical = 0;
+            for(int i=5;i<10;i++){
+                defensaParical+=defensa[i];
+            }
+            if(defensaParical>recordDefensa){
+                nombres = formacion;
+            } // falta agregar esto, "If there is still more than one combination, pick the attackers that come lexicographically earliest."
+            return;
+        }
+        nombres = formacion;
+        return;
+    }
+    
+}
+
+int main(){
+    int casos;
+    cin >> casos;
+    vector<string>nombres;
+    vector<int> ataque;
+    vector<int> defensa;
+    string nombre;
+    int score;
+    for(int i=0;i<10;i++){
+        cin>> nombre;
+        nombres.push_back(nombre);
+        cin>>score;
+        ataque.push_back(score);
+        cin>>score;
+        defensa.push_back(score);
+    }
+
+    return 0;
+}
