b1565197eb369dd2ae4282151a2e59b21786ef5c
TobiPerel <pereltobias@gmail.com>
2024-03-26 16:01:10 -0300

    cambios

--- backtracking/maradona.cpp
+++ backtracking/maradona.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 
 
-void encontrarMejorFormacion(vector<string>&nombres,vector<int>ataque,vector<int>defensa, vector<string>&formacion,int sumaAtacantes, int sumaDefensas,int recordAtaque, int recordDefensa, int index){
-    if(index == 4 && sumaAtacantes>=recordAtaque){
-        if(sumaAtacantes==recordAtaque){
-            int defensaParical = 0;
-            for(int i=5;i<10;i++){
-                defensaParical+=defensa[i];
-            }
-            if(defensaParical>recordDefensa){
-                nombres = formacion;
-            } // falta agregar esto, "If there is still more than one combination, pick the attackers that come lexicographically earliest."
-            return;
+void encontrarMejorFormacion(vector<string>nombres,vector<int>ataque,vector<int>defensa, vector<string>&parcial,vector<string> ganadorProvisorio,int sumaAtacantes,int recordAtaque, int index){
+    if(parcial.size() == 5 ){
+        if (sumaAtacantes > recordAtaque || sumaAtacantes == recordAtaque){
+            recordAtaque = sumaAtacantes;
+            ganadorProvisorio = parcial;
+            
         }
-        nombres = formacion;
         return;
     }
-    for(int i=index;i<10;i++){
-        if(sumaAtacantes>)
+    for(int i = index;i<10;i++){
+        parcial.push_back(nombres[i]);
+        encontrarMejorFormacion(nombres,ataque,defensa,parcial,ganadorProvisorio,sumaAtacantes+ataque[i],recordAtaque,index +1);
+        parcial.pop_back();
     }
+    
+    
+
 }
 
 int main(){
@@ -31,6 +30,9 @@ int main(){
     vector<int> defensa;
     string nombre;
     int score;
+    vector<string> parcial; // Add this line
+    vector<string> ganadorProvisorio;
+
     for(int i=0;i<10;i++){
         cin>> nombre;
         nombres.push_back(nombre);
@@ -39,6 +41,10 @@ int main(){
         cin>>score;
         defensa.push_back(score);
     }
-
+    int sumaTotal = 0;
+    for(int i =0; i<10;i++){
+        sumaTotal+= ataque[i];
+    }
+    encontrarMejorFormacion(nombres,ataque,defensa,parcial,ganadorProvisorio,0,0,0);
     return 0;
 }
