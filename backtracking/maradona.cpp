b4ee3cb1ae36d58c1157e24c3bd96d5ec014d46b
TobiPerel <pereltobias@gmail.com>
2024-03-27 18:54:14 -0300

    parcialmente terminado

--- backtracking/maradona.cpp
+++ backtracking/maradona.cpp
@@ -4,55 +4,106 @@ using namespace std;
 
 int recordAtaque = 0;
 int recordDefensa = 0;
-vector<string> ganadorProvisorio;
 
-void encontrarMejorFormacion(vector<string> nombres, vector<int> ataque,
-                             vector<int> defensa, vector<string> &parcial,
-                             int sumaAtacantes, int sumaDefensa, int index) {
-  if (parcial.size() == 5) {
-    if (sumaAtacantes > recordAtaque ||
-        (sumaAtacantes == recordAtaque) & (sumaDefensa > recordDefensa)) {
-      recordAtaque = sumaAtacantes;
-      recordDefensa = sumaDefensa;
-      ganadorProvisorio = parcial;
+
+struct jugador
+{
+    string nombre;
+    int ataque;
+    int defensa;
+    bool operator<(const jugador& other) const {
+        return nombre < other.nombre;
     }
-    return;
-  }
-  for (int i = index; i < 10; i++) {
-    parcial.push_back(nombres[i]);
-    encontrarMejorFormacion(nombres, ataque, defensa, parcial,
-                            sumaAtacantes + ataque[i], sumaDefensa + defensa[i],
-                            i + 1);
-    parcial.pop_back();
-  }
+};
+
+vector<jugador> jugadores;
+vector<jugador> ganadorProvisorio;
+
+
+bool compareFormations(const vector<jugador>& a, const vector<jugador>& b) {
+        return a[0].nombre < b[0].nombre;
 }
 
-int main() {
-  int casos;
-  cin >> casos;
-  vector<string> nombres;
-  vector<int> ataque;
-  vector<int> defensa;
-  string nombre;
-  int score;
-  vector<string> parcial; // Add this line
+void bubbleSort(vector<jugador> &jugadores, int posicion)
+{
+    for (int i = 0+posicion; i < 5+posicion; i++)
+    {
+        for (int j = 0+posicion; j < 4 - i+posicion; j++)
+        {
+            if (jugadores[j].nombre > jugadores[j + 1].nombre)
+            {
+                jugador temp = jugadores[j];
+                jugadores[j] = jugadores[j + 1];
+                jugadores[j + 1] = temp;
+            }
+        }
+    }
+}
 
-  for (int i = 0; i < 10; i++) {
-    cin >> nombre;
-    nombres.push_back(nombre);
-    cin >> score;
-    ataque.push_back(score);
-    cin >> score;
-    defensa.push_back(score);
-  }
-  int sumaTotal = 0;
-  for (int i = 0; i < 10; i++) {
-    sumaTotal += ataque[i];
-  }
-  encontrarMejorFormacion(nombres, ataque, defensa, parcial, 0, 0, 0);
-  cout << "Mejor formación: ";
-  for (int i = 0; i < 5; i++) {
-    cout << ganadorProvisorio[i] << " ";
-  }
-  return 0;
+
+void encontrarMejorFormacion(vector<jugador>jugadores , vector<jugador> &parcial,int sumaAtacantes, int sumaDefensa, int index)
+{
+
+    if (parcial.size() == 5)
+    {
+        if (sumaAtacantes > recordAtaque ||
+            (sumaAtacantes == recordAtaque) & (sumaDefensa > recordDefensa) || (sumaAtacantes == recordAtaque && sumaDefensa == recordDefensa &&  compareFormations(parcial, ganadorProvisorio)   ) ){
+            recordAtaque = sumaAtacantes;
+            recordDefensa = sumaDefensa;
+            ganadorProvisorio = parcial;
+        }
+        return;
+    }
+    for (int i = index; i < 10; i++)
+    {
+        parcial.push_back(jugadores[i]);
+        encontrarMejorFormacion(jugadores, parcial,
+                                sumaAtacantes + jugadores[i].ataque, sumaDefensa + jugadores[i].defensa,
+                                i + 1);
+        parcial.pop_back();
+    }
+}
+
+int main()
+{
+    int casos;
+    cin >> casos;
+    string nombre;
+    int ataque;
+    int defensa;
+    vector<jugador> parcial; // Add this line
+
+    for (int i = 0; i < 10; i++)
+    {
+        string nombre; // Add this line
+        cin >> nombre; // Change jugador.name to nombre
+        cin >> ataque;
+        cin >> defensa;
+        jugadores.push_back(jugador{nombre, ataque, defensa});
+    }
+    
+    encontrarMejorFormacion(jugadores, parcial, 0, 0, 0);
+    bubbleSort(ganadorProvisorio,0);
+
+    // agergar a los 5 jugadores restantes
+    for (int i=0; i <10;i++){
+        bool found = false;
+        for (int j=0; j<10;j++){
+            if (jugadores[i].nombre == ganadorProvisorio[j].nombre){
+                found = true;
+                break;
+            }
+        }
+        if (!found){
+            ganadorProvisorio.push_back(jugadores[i]);
+        }
+    }
+
+    bubbleSort(ganadorProvisorio,5);
+    cout << "Mejor formacion: ";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << ganadorProvisorio[i].nombre << " ";
+    }
+    return 0;
 }
