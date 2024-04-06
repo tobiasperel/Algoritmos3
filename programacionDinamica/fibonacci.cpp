25682d1256c4311120e737055a2b89c00cb4b3c4
TobiPerel <pereltobias@gmail.com>
2024-04-06 20:06:10 -0300

    astro

--- programacionDinamica/fibonacci.cpp
+++ programacionDinamica/fibonacci.cpp
@@ -11,24 +11,41 @@ int sucesionFibonacciBack(int n){
     return sucesionFibonacciBack(n-1) + sucesionFibonacciBack(n-2);
 }
 
-int sucesionFibonacciDP(int n){
+int sucesionFibonacciDPTopDown(int n){
     if(n == 0 || n == 1){
         return 1;
     }
 
     if(numeros[n-1]==-1){
-        numeros[n-1] = sucesionFibonacciDP(n-1) + sucesionFibonacciDP(n-2);
+        numeros[n-1] = sucesionFibonacciDPTopDown(n-1) + sucesionFibonacciDPTopDown(n-2);
     }
     return numeros[n-1];
 }
 
-int main(){
-    int numeroSecuencia = 35;
-    cout<<sucesionFibonacciBack(numeroSecuencia)<<endl;
+int sucesionFibonacciDPBottomUp(int n,int index) {
+    
+    if(index == n){
+        return numeros[n];
+    }
+    numeros[index+2] = numeros[index] + numeros[index+1];
+    return sucesionFibonacciDPBottomUp(n,index+1);
 
-    for(int i = 0; i<numeroSecuencia;i++){
+}
+
+int main(){
+    int numeroSecuencia = 9;
+    // cout<<sucesionFibonacciBack(numeroSecuencia)<<endl;
+
+    for(int i = 0; i<numeroSecuencia-1;i++){
         numeros.push_back(-1);
     }
-    cout<<sucesionFibonacciDP(numeroSecuencia);
+    cout<<sucesionFibonacciDPTopDown(numeroSecuencia-1);
+    cout<<endl;
+    for(int i = 0; i<numeroSecuencia-1;i++){
+        numeros[i] = -1;
+    }
+    numeros[0] = 1;
+    numeros[1] = 1;
+    cout<<sucesionFibonacciDPBottomUp(numeroSecuencia-1,0);
     return 0;
 }
