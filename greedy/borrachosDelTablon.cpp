a0228cf0346a8490ba48e1c5577dc963632f181f
tobiHonduras <pereltobias@gmail.com>
2024-04-19 18:17:51 -0300

    ckechpoint

--- greedy/borrachosDelTablon.cpp
+++ greedy/borrachosDelTablon.cpp
@@ -1,33 +1,64 @@
-#include<iostream>
-#include<vector>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-
-int vinoGoloso(vector<int> borrachos){
-    int costo = 0;
-    int parcial = 0;
-    for(int i=0;i<borrachos.size();i++){
-        borrachos[i] += parcial;
-        parcial = borrachos[i];
-        borrachos[i] = 0;
-        costo += abs(parcial);
+int longitud;
+vector<int> pasos;
+vector<int> poscionesABorrar;
+void cambiarLetras(string &p1, string &p2) {
+  for (int i = 0; i < longitud; i++) {
+    if (p1[i] == p2[i]) {
+      poscionesABorrar.push_back(i);
+      continue;
     }
-    return costo;
-}
-
-int main(){
-    int habitantes;
-    cin >> habitantes;
-    string superFrase = "";
-    while (habitantes != 0) {
-        vector<int> borrachos(habitantes);
-        for(int j=0; j<habitantes; j++){
-            cin >> borrachos[j];
-        }
-        vinoGoloso(borrachos);
-        superFrase+= to_string(vinoGoloso(borrachos)) + "\n";
-        cin >> habitantes;
+  }
+  for (int i = 0; i < poscionesABorrar.size(); i++) {
+    p1.erase(poscionesABorrar[i], 1);
+    p2.erase(poscionesABorrar[i], 1);
+  }
+  for (int i = 0; i < p1.size(); i++) {
+    if (p1[i] != p2[i + 1]) {
+      char aux = p1[i];
+      p1[i] = p2[i + 1];
+      p2[i + 1] = aux;
+      pasos.push_back(i + 1);
+      pasos.push_back(i + 2);
     }
-    cout<<superFrase;
+  }
+
+  int main() {
+
+    cin >> longitud;
+    string p1, p2;
+    cin >> p1;
+    cin >> p2;
+    int a1 = 0;
+    int a2 = 0;
+    int b1 = 0;
+    int b2 = 0;
+
+    for (int i = 0; i < longitud; i++) {
+      if (p1[i] == 'a') {
+        a1++;
+      } else {
+        b1++;
+      }
+      if (p2[i] == 'a') {
+        a2++;
+      } else {
+        b2++;
+      }
+    }
+    if ((a1 + a2) % 2 == 1 || (b1 + b2) % 2 == 1) {
+      cout << -1; // Error de entrada. No es una permut
+    } else {
+      cambiarLetras(p1, p2);
+      cout << pasos.size() / 2 << endl;
+      for (int i = 0; i < pasos.size(); i += 2) {
+        cout << pasos[i] << " " << pasos[i + 1] << endl;
+      }
+      cout << p1 << endl << p2;
+    }
     return 0;
-}
+  }
