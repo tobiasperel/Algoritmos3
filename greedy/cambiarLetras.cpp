410ae6ad84967bc8eab2153298cdf0f350adb6d1
TobiPerel <pereltobias@gmail.com>
2024-04-21 23:01:47 -0300

    probando

--- greedy/cambiarLetras.cpp
+++ greedy/cambiarLetras.cpp
@@ -1,78 +1,114 @@
+#include <algorithm>
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
 
 int longitud;
 vector<int> pasos;
+void cambiarLetras(string &p1, string &p2)
+{
 
-void cambiarLetras(string p1, string p2) {
-  for (int i = 0; i < longitud; i++) {
-
-    if (p1[i] == p2[i]) {
-      continue;
+    vector<bool> p1A(p1.size(),false);
+    vector<bool> p2A(p2.size(),false);
+    for (int i = 0; i < p1.size(); i++)
+    {
+        if (p1[i] != p2[i])
+        {
+            if (p1[i] == 'a')
+            {
+                p1A[i] = true;
+            }
+            else
+            {
+                p2A[i] = true;
+            }
+        }
     }
-    if (p1[i] != p2[i + 1]) {
-      char aux = p1[i];
-      p1[i] = p2[i + 1];
-      p2[i + 1] = aux;
-      pasos.push_back(i + 1);
-      pasos.push_back(i + 2);
+    queue<int>interinA;
+    queue<int>interinB;
+    for (int i = 0; i < p1.size(); i++)
+    {
+        if (p1[i] != p2[i])
+        {
+            if(p1A[i] == true){
+                if(interinB.empty()){
+                    interinA.push(i);
+                }
+                else{
+                    char aux = p2[interinB.front()];
+                    p2[interinB.front()] = p1[i];
+                    p1[i] = aux;
+                    pasos.push_back(i);
+                    pasos.push_back(interinB.front());
+                    interinA.pop();
+                    interinB.pop();
+                    
+                }
+            }
+            else{
+                if(interinA.empty()){
+                    interinB.push(i);
+                }
+                else{
+                    char aux = p2[interinA.front()];
+                    p2[interinA.front()] = p1[i];
+                    p1[i] = aux;
+                    pasos.push_back(i);
+                    pasos.push_back(interinA.front());
+                    interinA.pop();
+                    interinB.pop();
+                    
+                }
+            }
+        }
     }
-  }
 }
+int main()
+{
 
-void nivelar(string &p1, string &p2, int cantidadDeCambios, bool masAEnp1) {
-  for (int i = 0; i < longitud; i++) {
-    if (p1[i] != p2[i] && cantidadDeCambios > 0) {
-      cantidadDeCambios--;
-      if ((p1[i] == 'a' && masAEnp1) || (p1[i] == 'b' && masAEnp1 == false)) {
-        char aux = p2[i];
-        p2[i] = p1[i];
-        p1[i] = aux;
-        pasos.push_back(i);
-        pasos.push_back(i);
-      }
-    }
-  }
-}
+    cin >> longitud;
+    string p1, p2;
+    cin >> p1;
+    cin >> p2;
+    int a1 = 0;
+    int a2 = 0;
+    int b1 = 0;
+    int b2 = 0;
 
-int main() {
-
-  cin >> longitud;
-  string p1, p2;
-  cin >> p1;
-  cin >> p2;
-  int a1 = 0;
-  int a2 = 0;
-  int b1 = 0;
-  int b2 = 0;
-
-  for (int i = 0; i < longitud; i++) {
-    if (p1[i] == 'a') {
-      a1++;
-    } else {
-      b1++;
+    for (int i = 0; i < longitud; i++)
+    {
+        if (p1[i] == 'a')
+        {
+            a1++;
+        }
+        else
+        {
+            b1++;
+        }
+        if (p2[i] == 'a')
+        {
+            a2++;
+        }
+        else
+        {
+            b2++;
+        }
     }
-    if (p2[i] == 'a') {
-      a2++;
-    } else {
-      b2++;
+    if ((a1 + a2) % 2 == 1 || (b1 + b2) % 2 == 1)
+    {
+        cout << -1; // Error de entrada. No es una permut
     }
-  }
-  if ((a1 + a2) % 2 == 1 || (b1 + b2) % 2 == 1) {
-    cout << -1; // Error de entrada. No es una permut
-  } else {
-    if (a1 == a2 && b1 == b2) {
-      // cambiarLetras(p1, p2);
-    } else {
-      nivelar(p1, p2, abs(a1 - a2 / 2), a1 > a2);
-      // cambiarLetras(p1, p2);
+    else
+    {
+        cambiarLetras(p1, p2);
+        cout << pasos.size() / 2 << endl;
+        for (int i = 0; i < pasos.size(); i += 2)
+        {
+            cout << pasos[i] << " " << pasos[i + 1] << endl;
+        }
+        cout << p1 << endl
+             << p2;
     }
-    cout << pasos.size() / 2 << endl;
-    for (int i = 0; i < pasos.size(); i += 2) {
-      cout << pasos[i] << " " << pasos[i + 1] << endl;
-    }
-    cout << p1 << endl << p2;
-  }
-  return 0;
+    return 0;
 }
